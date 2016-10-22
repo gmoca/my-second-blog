ff517045dc3a3d6c9a436b882e6bde5ab32337cc
gmoca <gustavmontiel@outlook.com>
2016-10-22 17:02:05 -0500

    se modifico blog/viewsy blog/urls para el uso de CBV en reemplZO DE fbv

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.article_list_view, name='blog.article_list'),
+    # url(r'^$', views.article_list_view, name='blog.article_list'),
+    url(r'^$', views.ArticleListView.as_view(), name='blog.article_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from .models import Article
+from django.views import generic
 # Create your views here.
 
 
-def article_list_view(request):
-    articles = Article.objects.all()
-    context = {'articles': articles}
-    print(context)
-    return render(request, 'blog/article_list.html', context)
+# def article_list_view(generic.ListView):
+class ArticleListView(generic.ListView):
+    template_model = 'bloc/article_list.html'
+    model = Article
+    context_object_name = 'articles'
